86618483a89354e3111b7f3c1710899aca114c03
Krishnaprasad <krishnaprasadj@gmail.com>
2018-06-19 12:04:23 +0000

    Adding create functionality

---
+++ WebApplication1/WebApplication1/Pages/Create.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _dbContext;
+
+        //Need to bind data
+        [BindProperty]
+        public Employee Employee { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public CreateModel(AppDbContext dbContext) { _dbContext = dbContext; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //Add the employee to the list
+            _dbContext.Employees.Add(Employee);
+
+            //Save changes
+            await _dbContext.SaveChangesAsync();
+
+            Message = @"Employee {Employee.Name} added successfully.";
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
--- WebApplication1/WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/Index.cshtml.cs
@@ -4,14 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly AppDbContext _dbContext;
 
+        //Need to bind data
+        [BindProperty]
+        public IList<Employee> Employees { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public IndexModel(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+
+        }
+
+        public async Task OnGetAsync()
+        {
+            Employees = await _dbContext.Employees.AsNoTracking().ToListAsync();
         }
     }
 }
