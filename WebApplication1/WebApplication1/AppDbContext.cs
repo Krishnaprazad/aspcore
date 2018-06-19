21067dc7c87700f9fa73325f7613e2cd8e5cfde7
Krishnaprasad <krishnaprasadj@gmail.com>
2018-06-19 11:41:48 +0000

    Initial checkin

---
+++ WebApplication1/WebApplication1/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Create Employee list
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1
+{
+    public class Employee
+
+    {
+        public int EmpId { get; set; }
+
+        //Minimal length for employee name
+        [Required, StringLength(5)]
+        public string Name { get; set; }
+    }
+}
